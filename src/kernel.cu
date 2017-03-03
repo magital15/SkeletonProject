11075db6285ac944f2cd9332de52f358f6a7b199
Cody Moody <cjmoody@uw.edu>
2017-03-02 22:36:39 -0800

    Update kernel.cu

--- src/kernel.cu
+++ src/kernel.cu
@@ -306,11 +306,10 @@ void scalarMultPoly(Poly in, Poly out, int scalar, int* primes)
 
 void subtractPolys(Poly a, Poly b, Poly c, int* primes)
 {
-//	scalarMultPoly(b, c, -1, primes);
-//	addPolys(a, c, c, primes);
+	scalarMultPoly(b, c, -1, primes);
+	addPolys(a, c, c, primes);
 
-//	What to do with negative numbers....
-
+//	Above is same as Below?
 
 /*
 	int len = a.length;
@@ -351,3 +350,41 @@ void subtractPolys(Poly a, Poly b, Poly c, int* primes)
 */
 }
 
+void multiplyPolys(Poly a, Poly b, Poly c, int* primes)
+{
+	int len = c.length;
+
+	// Declare pointers to device arrays
+	int *d_a = 0;
+	int *d_b = 0;
+	int *d_out = 0;
+
+	//Allocate memory for device arrays
+	cudaMalloc(&d_a, len*sizeof(int));
+	cudaMalloc(&d_b, len*sizeof(int));
+	cudaMalloc(&d_out, len*sizeof(int));
+
+	// Do this for all polys in Polyset
+	for (int i = 1; i < NUMPRIMES+1; i++) {
+		// Copy input data from host to device
+		cudaMemcpy(d_a, a.members[i].coeffs, len*sizeof(int), 
+				   cudaMemcpyHostToDevice);
+		cudaMemcpy(d_b, b.members[i].coeffs, len*sizeof(int), 
+				   cudaMemcpyHostToDevice);
+  
+		// Launch kernel to compute and store modded polynomial values
+		minusMult<<<(len + TPB - 1)/TPB, TPB>>>(d_out, d_b, -1, 
+											    primes[i-1]);
+		addMods<<<(len + TPB - 1)/TPB, TPB>>>(d_out, d_a, d_out, 
+											  primes[i-1]);
+
+		// Copy results from device to host
+		cudaMemcpy(c.members[i].coeffs, d_out, len*sizeof(int), 
+				   cudaMemcpyDeviceToHost);
+	}
+  
+	// Free the memory allocated for device arrays
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_out);
+}
